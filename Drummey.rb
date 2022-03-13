c6587f15d3fef397b0f5a8ab630331add61ff7b5
senjellam <roman.herold@outlook.com>
2022-03-14 00:12:29 +0100

    implemented recording function

--- Drummey.rb
+++ Drummey.rb
@@ -1,8 +1,57 @@
 use_osc "localhost", 4560
 
 
-/ =================================================================== /
-/ GLOBAL VALUES /
+# ===================================================================
+# RECORDING (code by robin.newman)
+
+define :pvalue do #get current listen port for Sonic Pi from log file
+  value= 4557 #pre new logfile format port was always 4557
+  File.open(ENV['HOME']+'/.sonic-pi/log/server-output.log','r') do |f1|
+    while l = f1.gets
+      if l.include?"Listen port:"
+        value = l.split(" ").last.to_i
+        break
+      end
+    end
+    f1.close
+  end
+  return value
+end
+
+puts "Server Listen port is: #{pvalue}"
+set :pvalue,pvalue
+
+#three functions with will start recording, stop recording and save recorded audio file
+define :recordStart do #this command is equivalent to pushing the start recording button
+  use_real_time
+  pvalue=get(:pvalue)
+  osc_send "localhost",pvalue, "/start-recording","guid-rbn"
+  sleep 1# make sure recording running before creating any audio to save
+  puts "recording started"
+end
+
+define :recordStop do #this command stops a currently recording process
+  use_real_time
+  pvalue=get(:pvalue)
+  osc_send "localhost",pvalue, "/stop-recording","guid-rbn"
+end
+
+define :saveAudio do |file|  #this command saves the recorded audio file
+  pvalue=get(:pvalue)
+  osc_send "localhost",pvalue, "/save-recording","guid-rbn",file
+  puts "recording stopped"
+end
+
+#combine stop and save functions
+define :stopAndSaveRecording do |file|
+  recordStop
+  saveAudio(file)
+  puts "Recording saved to #{file}"
+end
+
+
+# ===================================================================
+# GLOBAL VALUES
 
 live_loop :getThreshold do
   use_real_time
@@ -23,8 +72,22 @@ live_loop :getRelease do
 end
 
 
-/ =================================================================== /
-/ FUSION DRUMS /
+# ===================================================================
+# START RECORDING
+
+live_loop :getStart do
+  use_real_time
+  setStart = sync "/osc*/setStart"
+  set :gStart, setStart[0]
+  startRec = get(:gStart)
+  if (startRec == 1)
+    recordStart
+  end
+end
+
+
+# ===================================================================
+# FUSION DRUMS
 
 live_loop :fusionBass do
   use_real_time
@@ -76,8 +139,8 @@ live_loop :fusionTomTom3 do
 end
 
 
-/ =================================================================== /
-/ ROCK DRUMS /
+# ===================================================================
+# ROCK DRUMS
 
 live_loop :rockBass do
   use_real_time
@@ -136,8 +199,8 @@ live_loop :rockCrash do
 end
 
 
-/ =================================================================== /
-/ JAZZ DRUMS /
+# ===================================================================
+# JAZZ DRUMS
 
 live_loop :jazzBass do
   use_real_time
@@ -179,4 +242,18 @@ live_loop :jazzHiHat do
   drumsJazz = sync "/osc*/drumsJazz"
   set :amp, drumsJazz[5]
   sample :drum_cymbal_closed, amp: get(:amp), threshold: get(:gThreshold), attack: get(:gAttack), release: get(:gRelease)
+end
+
+
+# ===================================================================
+# STOP RECORDING
+
+live_loop :getStop do
+  use_real_time
+  setStop = sync "/osc*/setStop"
+  set :gStop, setStop[0]
+  stopRec = get(:gStop)
+  if (stopRec == 1)
+    stopAndSaveRecording("D:/Hochschule/05_Wintersemester_2021-2022/Abschlussprojekte/Drummey/Drummey/Drummey_Recording.wav")
+  end
 end
