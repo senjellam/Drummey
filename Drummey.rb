9a15042640dc2e73dbb3cf83f59124491f7f719b
senjellam <roman.herold@outlook.com>
2022-03-14 19:37:07 +0100

    connected knob values to samples, changed how setPath works

--- Drummey.rb
+++ Drummey.rb
@@ -53,12 +53,6 @@ end
 # ===================================================================
 # GLOBAL VALUES
 
-live_loop :getThreshold do
-  use_real_time
-  setThreshold = sync "/osc*/setThreshold"
-  set :gThreshold, setThreshold[0]
-end
-
 live_loop :getAttack do
   use_real_time
   setAttack = sync "/osc*/setAttack"
@@ -69,6 +63,18 @@ live_loop :getRelease do
   use_real_time
   setRelease = sync "/osc*/setRelease"
   set :gRelease, setRelease[0]
+end
+
+live_loop :getSustain do
+  use_real_time
+  setSustain = sync "/osc*/setSustain"
+  set :gSustain, setSustain[0]
+end
+
+live_loop :getRate do
+  use_real_time
+  setRate = sync "/osc*/setRate"
+  set :gRate, setRate[0]
 end
 
 
@@ -93,49 +99,49 @@ live_loop :fusionBass do
   use_real_time
   drumsFusion = sync "/osc*/drumsFusion"
   set :amp, drumsFusion[0]
-  sample :drum_bass_hard, amp: get(:amp), threshold: get(:gThreshold), attack: get(:gAttack), release: get(:gRelease)
+  sample :drum_bass_hard, amp: get(:amp), attack: get(:gAttack), release: get(:gRelease), sustain: get(:gSustain), rate: get(:gRate)
 end
 
 live_loop :fusionSnare do
   use_real_time
   drumsFusion = sync "/osc*/drumsFusion"
   set :amp, drumsFusion[1]
-  sample :drum_snare_hard, amp: get(:amp), threshold: get(:gThreshold), attack: get(:gAttack), release: get(:gRelease)
+  sample :drum_snare_hard, amp: get(:amp), attack: get(:gAttack), release: get(:gRelease), sustain: get(:gSustain), rate: get(:gRate)
 end
 
 live_loop :fusionTomTom1 do
   use_real_time
   drumsFusion = sync "/osc*/drumsFusion"
   set :amp, drumsFusion[2]
-  sample :drum_tom_mid_hard, amp: get(:amp), threshold: get(:gThreshold), attack: get(:gAttack), release: get(:gRelease)
+  sample :drum_tom_mid_hard, amp: get(:amp), attack: get(:gAttack), release: get(:gRelease), sustain: get(:gSustain), rate: get(:gRate)
 end
 
 live_loop :fusionTomTom2 do
   use_real_time
   drumsFusion = sync "/osc*/drumsFusion"
   set :amp, drumsFusion[3]
-  sample :drum_tom_hi_hard, amp: get(:amp), threshold: get(:gThreshold), attack: get(:gAttack), release: get(:gRelease)
+  sample :drum_tom_hi_hard, amp: get(:amp), attack: get(:gAttack), release: get(:gRelease), sustain: get(:gSustain), rate: get(:gRate)
 end
 
 live_loop :fusionRide do
   use_real_time
   drumsFusion = sync "/osc*/drumsFusion"
   set :amp, drumsFusion[4]
-  sample :drum_cymbal_hard, amp: get(:amp), threshold: get(:gThreshold), attack: get(:gAttack), release: get(:gRelease)
+  sample :drum_cymbal_hard, amp: get(:amp), attack: get(:gAttack), release: get(:gRelease), sustain: get(:gSustain), rate: get(:gRate)
 end
 
 live_loop :fusionHiHat do
   use_real_time
   drumsFusion = sync "/osc*/drumsFusion"
   set :amp, drumsFusion[5]
-  sample :drum_cymbal_closed, amp: get(:amp), threshold: get(:gThreshold), attack: get(:gAttack), release: get(:gRelease)
+  sample :drum_cymbal_closed, amp: get(:amp), attack: get(:gAttack), release: get(:gRelease), sustain: get(:gSustain), rate: get(:gRate)
 end
 
 live_loop :fusionTomTom3 do
   use_real_time
   drumsFusion = sync "/osc*/drumsFusion"
   set :amp, drumsFusion[6]
-  sample :drum_tom_lo_hard, amp: get(:amp), threshold: get(:gThreshold), attack: get(:gAttack), release: get(:gRelease)
+  sample :drum_tom_lo_hard, amp: get(:amp), attack: get(:gAttack), release: get(:gRelease), sustain: get(:gSustain), rate: get(:gRate)
 end
 
 
@@ -146,56 +152,57 @@ live_loop :rockBass do
   use_real_time
   drumsRock = sync "/osc*/drumsRock"
   set :amp, drumsRock[0]
-  sample :drum_bass_hard, amp: get(:amp), threshold: get(:gThreshold), attack: get(:gAttack), release: get(:gRelease)
+  sample :drum_bass_hard, amp: get(:amp), attack: get(:gAttack), release: get(:gRelease), sustain: get(:gSustain), rate: get(:gRate)
 end
 
 live_loop :rockSnare do
   use_real_time
   drumsRock = sync "/osc*/drumsRock"
   set :amp, drumsRock[1]
-  sample :drum_snare_hard, amp: get(:amp), threshold: get(:gThreshold), attack: get(:gAttack), release: get(:gRelease)
+  sample :drum_snare_hard, amp: get(:amp), attack: get(:gAttack), release: get(:gRelease), sustain: get(:gSustain), rate: get(:gRate)
 end
 
 live_loop :rockTomTom1 do
   use_real_time
   drumsRock = sync "/osc*/drumsRock"
   set :amp, drumsRock[2]
-  sample :drum_tom_mid_hard, amp: get(:amp), threshold: get(:gThreshold), attack: get(:gAttack), release: get(:gRelease)
+  sample :drum_tom_mid_hard, amp: get(:amp), attack: get(:gAttack), release: get(:gRelease), sustain: get(:gSustain), rate: get(:gRate)
 end
 
 live_loop :rockTomTom2 do
   use_real_time
   drumsRock = sync "/osc*/drumsRock"
   set :amp, drumsRock[3]
-  sample :drum_tom_hi_hard, amp: get(:amp), threshold: get(:gThreshold), attack: get(:gAttack), release: get(:gRelease)
+  sample :drum_tom_hi_hard, amp: get(:amp), attack: get(:gAttack), release: get(:gRelease), sustain: get(:gSustain), rate: get(:gRate)
 end
 
 live_loop :rockRide do
   use_real_time
   drumsRock = sync "/osc*/drumsRock"
   set :amp, drumsRock[4]
-  sample :drum_cymbal_hard, amp: get(:amp), threshold: get(:gThreshold), attack: get(:gAttack), release: get(:gRelease)
+  sample :drum_cymbal_hard, amp: get(:amp), attack: get(:gAttack), release: get(:gRelease), sustain: get(:gSustain), rate: get(:gRate)
 end
 
 live_loop :rockHiHat do
   use_real_time
   drumsRock = sync "/osc*/drumsRock"
   set :amp, drumsRock[5]
-  sample :drum_cymbal_closed, amp: get(:amp), threshold: get(:gThreshold), attack: get(:gAttack), release: get(:gRelease)
+  sample :drum_cymbal_closed, amp: get(:amp), attack: get(:gAttack), release: get(:gRelease), sustain: get(:gSustain), rate: get(:gRate)
 end
 
 live_loop :rockTomTom3 do
   use_real_time
   drumsRock = sync "/osc*/drumsRock"
   set :amp, drumsRock[6]
-  sample :drum_tom_lo_hard, amp: get(:amp), threshold: get(:gThreshold), attack: get(:gAttack), release: get(:gRelease)
+  sample :drum_tom_lo_hard, amp: get(:amp), attack: get(:gAttack), release: get(:gRelease), sustain: get(:gSustain), rate: get(:gRate)
 end
 
 live_loop :rockCrash do
   use_real_time
   drumsRock = sync "/osc*/drumsRock"
   set :amp, drumsRock[7]
-  sample :drum_cymbal_open, amp: get(:amp), threshold: get(:gThreshold), attack: get(:gAttack), release: get(:gRelease)
+  # sample :drum_cymbal_open, amp: get(:amp), attack: 0.2, sustain: 0.5, release: 0.7, rate: 1
+  sample :drum_cymbal_open, amp: get(:amp), attack: get(:gAttack), release: get(:gRelease), sustain: get(:gSustain), rate: get(:gRate)
 end
 
 
@@ -206,42 +213,42 @@ live_loop :jazzBass do
   use_real_time
   drumsJazz = sync "/osc*/drumsJazz"
   set :amp, drumsJazz[0]
-  sample :drum_bass_hard, amp: get(:amp), threshold: get(:gThreshold), attack: get(:gAttack), release: get(:gRelease)
+  sample :drum_bass_hard, amp: get(:amp), attack: get(:gAttack), release: get(:gRelease), sustain: get(:gSustain), rate: get(:gRate)
 end
 
 live_loop :jazzSnare do
   use_real_time
   drumsJazz = sync "/osc*/drumsJazz"
   set :amp, drumsJazz[1]
-  sample :drum_snare_hard, amp: get(:amp), threshold: get(:gThreshold), attack: get(:gAttack), release: get(:gRelease)
+  sample :drum_snare_hard, amp: get(:amp), attack: get(:gAttack), release: get(:gRelease), sustain: get(:gSustain), rate: get(:gRate)
 end
 
 live_loop :jazzTomTom1 do
   use_real_time
   drumsJazz = sync "/osc*/drumsJazz"
   set :amp, drumsJazz[2]
-  sample :drum_tom_mid_hard, amp: get(:amp), threshold: get(:gThreshold), attack: get(:gAttack), release: get(:gRelease)
+  sample :drum_tom_mid_hard, amp: get(:amp), attack: get(:gAttack), release: get(:gRelease), sustain: get(:gSustain), rate: get(:gRate)
 end
 
 live_loop :jazzTomTom2 do
   use_real_time
   drumsJazz = sync "/osc*/drumsJazz"
   set :amp, drumsJazz[3]
-  sample :drum_tom_hi_hard, amp: get(:amp), threshold: get(:gThreshold), attack: get(:gAttack), release: get(:gRelease)
+  sample :drum_tom_hi_hard, amp: get(:amp), attack: get(:gAttack), release: get(:gRelease), sustain: get(:gSustain), rate: get(:gRate)
 end
 
 live_loop :jazzRide do
   use_real_time
   drumsJazz = sync "/osc*/drumsJazz"
   set :amp, drumsJazz[4]
-  sample :drum_cymbal_hard, amp: get(:amp), threshold: get(:gThreshold), attack: get(:gAttack), release: get(:gRelease)
+  sample :drum_cymbal_hard, amp: get(:amp), attack: get(:gAttack), release: get(:gRelease), sustain: get(:gSustain), rate: get(:gRate)
 end
 
 live_loop :jazzHiHat do
   use_real_time
   drumsJazz = sync "/osc*/drumsJazz"
   set :amp, drumsJazz[5]
-  sample :drum_cymbal_closed, amp: get(:amp), threshold: get(:gThreshold), attack: get(:gAttack), release: get(:gRelease)
+  sample :drum_cymbal_closed, amp: get(:amp), attack: get(:gAttack), release: get(:gRelease), sustain: get(:gSustain), rate: get(:gRate)
 end
 
 
@@ -253,7 +260,10 @@ live_loop :getStop do
   setStop = sync "/osc*/setStop"
   set :gStop, setStop[0]
   stopRec = get(:gStop)
+  setPath = sync "/osc*/setPath"
+  set :gPath, setPath[0]
+  savePath = get(:gPath)
   if (stopRec == 1)
-    stopAndSaveRecording("D:/Hochschule/05_Wintersemester_2021-2022/Abschlussprojekte/Drummey/Drummey/Drummey_Recording.wav")
+    stopAndSaveRecording(savePath)
   end
 end
